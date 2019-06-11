61cc96940d68792f1315c7f0a5478313775ecd8b
Your Name <you@example.com>
2019-06-11 15:24:56 +0800

    Initial Commit

---
+++ pages/demo/demo.js
@@ -0,0 +1,86 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: 'hello word!',
+    arr: [1,2,3,4,5],
+    demo: {
+      fristName: '田伟'
+    },
+    inputVal: ''
+  },
+
+  viewTap: function() {
+    console.log(this.data.msg);
+  },
+
+  changeMsg: function() {
+    this.setData({
+      msg: '已改变'
+    });
+  },
+
+  changeVal:function(e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
